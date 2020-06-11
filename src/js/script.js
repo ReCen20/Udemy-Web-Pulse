0381c26809c2256cec56738e0a75a8c277726793
ReCen20 <alpha55757@gmail.com>
2020-06-11 17:38:37 +0400

    add tab content

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,22 @@ document.querySelector('.carousel__prev').addEventListener('click', function () 
 document.querySelector('.carousel__next').addEventListener('click', function () {
     slider.goTo('next');
 });
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__main-content').eq(i).toggleClass('catalog-item__main-content_active');
+            $('.catalog-item__second-content').eq(i).toggleClass('catalog-item__second-content_active');
+        });
+    });
+}
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
